8ec0229d969ef79fe06c84fa79a940d9961777c1
archit girotra <archit.girotra@comviva.com>
2025-05-13 12:42:06 +0530

    feat: add Child and Parent components for color selection functionality

---
+++ app/callBack/childComp.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Child({
+  getColor,
+}: {
+  getColor: (color: string) => void;
+}) {
+  const [currentColor, setCurrentColor] = useState("white");
+  useEffect(() => {
+    getColor(currentColor);
+  }, [currentColor]);
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter color"
+        value={currentColor}
+        style={{
+          width: "200px",
+          height: "20px",
+          padding: "20px",
+          borderRadius: "5px",
+          border: "none",
+          backgroundColor: "rgba(255,255,255,0.3)",
+        }}
+        onChange={(e) => {
+          setCurrentColor(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
---
+++ app/callBack/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+import Child from "./childComp";
+
+export default function Parent() {
+  const [color, setColor] = useState("white");
+  const getColor = (color: any) => {
+    setColor(color);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: color,
+        width: "100vw",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Child getColor={getColor} />
+    </div>
+  );
+}
